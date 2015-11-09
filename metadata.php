640335654fdc7607123709f4fb478ee9aaa93268
Andrius Šilgalis <andrius.silgalis@gmail.com>
2015-11-09 20:22:45 +0200

    Add metadata.php

---
+++ metadata.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'oxid-symfony-dependency-injection',
+    'title'       => 'Symfony Dependency Injection for OXID eShop',
+    'description' => [
+        'en' => 'Symfony Dependency Injection for OXID eShop.',
+    ],
+    'author'      => 'Andrius Šilgalis',
+    'version'     => '0.0.1',
+];
