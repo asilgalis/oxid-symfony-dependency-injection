4e4399462ac5e9984499fcbcd65ff844cf220b0e
Andrius Šilgalis <andrius.silgalis@gmail.com>
2015-11-10 23:33:43 +0200

    Register ExtensionLoader

--- src_oxid/core/AsSymfonyDiShopControl.php
+++ src_oxid/core/AsSymfonyDiShopControl.php
@@ -1,16 +1,23 @@
 <?php
 
+use ASilgalis\OxidModule\SymfonyDi\DependencyInjection\ContainerAwareTrait;
+use ASilgalis\OxidModule\SymfonyDi\DependencyInjection\ExtensionLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 /**
  * @codeCoverageIgnore
  */
 class AsSymfonyDiShopControl extends AsSymfonyDiShopControl_parent
 {
+    use ContainerAwareTrait;
+
     /**
      * {@inheritdoc}
      */
     public function start($sClass = null, $sFunction = null, $aParams = null, $aViewsChain = null)
     {
         $this->registerContainerBuilder();
+        $this->registerExtensionLoader();
 
         parent::start($sClass, $sFunction, $aParams, $aViewsChain);
     }
@@ -20,6 +27,14 @@ class AsSymfonyDiShopControl extends AsSymfonyDiShopControl_parent
      */
     private function registerContainerBuilder()
     {
-        oxRegistry::set('container', new \Symfony\Component\DependencyInjection\ContainerBuilder());
+        oxRegistry::set('container', new ContainerBuilder());
+    }
+
+    private function registerExtensionLoader()
+    {
+        $extensionLoader = new ExtensionLoader();
+        $extensionLoader->setContainer($this->getContainer());
+
+        $this->getContainer()->set('as.extension_loader', $extensionLoader);
     }
 }
